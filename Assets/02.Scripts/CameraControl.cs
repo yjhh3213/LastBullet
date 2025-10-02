d159807253963b5c20126d40545aef5eb63da934
yjhh3213 <yjhh3213@gmail.com>
2025-10-02 13:19:46 +0900

    2025/10/02
    Camera 캐릭터에 맞춰서 움직이기

--- Assets/02.Scripts/CameraControl.cs
+++ Assets/02.Scripts/CameraControl.cs
@@ -5,15 +5,14 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
-    public GameObject player;
+    public Transform player;
     public float smoothing = 5.0f;
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector3 dir = player.transform.position - this.transform.position;
-        Vector3 move = new Vector3(dir.x * smoothing * Time.deltaTime, dir.y * smoothing * Time.deltaTime, 0.0f);
-        
-        this.transform.Translate(move);
+        Vector3 target = new Vector3(player.position.x, player.position.y, transform.position.z);
+
+        transform.position = Vector3.Lerp(transform.position, target, smoothing * Time.deltaTime);
     }
 }
